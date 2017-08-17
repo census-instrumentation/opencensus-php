5545ac2dd91da76ce10470255db3ef70ff8ab9f9
Jeff Ching <chingor@google.com>
2017-08-17 14:07:42 -0700

    Update ContextTracerTest to match the ExtensionTracerTest for maintaining context

--- tests/unit/Trace/Tracer/ContextTracerTest.php
+++ tests/unit/Trace/Tracer/ContextTracerTest.php
@@ -27,24 +27,25 @@ class ContextTracerTest extends \PHPUnit_Framework_TestCase
 {
     public function testMaintainsContext()
     {
-        $initialContext = new TraceContext('traceid', 'spanid');
+        $parentSpanId = 12345;
+        $initialContext = new TraceContext('traceid', $parentSpanId);
 
         $tracer = new ContextTracer($initialContext);
         $context = $tracer->context();
 
         $this->assertEquals('traceid', $context->traceId());
-        $this->assertEquals('spanid', $context->spanId());
+        $this->assertEquals($parentSpanId, $context->spanId());
 
-        $tracer->inSpan(['name' => 'test'], function() use ($tracer) {
+        $tracer->inSpan(['name' => 'test'], function() use ($tracer, $parentSpanId) {
             $context = $tracer->context();
-            $this->assertNotEquals('spanid', $context->spanId());
+            $this->assertNotEquals($parentSpanId, $context->spanId());
         });
 
         $spans = $tracer->spans();
         $this->assertCount(1, $spans);
         $span = $spans[0];
         $this->assertEquals('test', $span->name());
-        $this->assertEquals('spanid', $span->parentSpanId());
+        $this->assertEquals($parentSpanId, $span->parentSpanId());
     }
 
     public function testAddsLabelsToCurrentSpan()
