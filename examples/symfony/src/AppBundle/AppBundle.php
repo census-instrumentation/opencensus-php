86a1e75e60afc2b16f35c7409bb8ef5e1c63f1bd
Jeff Ching <chingor@google.com>
2017-11-17 10:26:00 -0800

    Enable OpenCensus in AppBundle boot

--- examples/symfony/src/AppBundle/AppBundle.php
+++ examples/symfony/src/AppBundle/AppBundle.php
@@ -2,8 +2,32 @@
 
 namespace AppBundle;
 
+use OpenCensus\Trace\Exporter\EchoExporter;
+use OpenCensus\Trace\Integrations\Mysql;
+use OpenCensus\Trace\Integrations\PDO;
+use OpenCensus\Trace\Integrations\Symfony;
+use OpenCensus\Trace\Tracer;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class AppBundle extends Bundle
 {
+    public function boot()
+    {
+        $this->setupOpenCensus();
+    }
+
+    private function setupOpenCensus()
+    {
+        if (php_sapi_name() == 'cli') {
+            return;
+        }
+
+        // Enable OpenCensus extension integrations
+        Mysql::load();
+        PDO::load();
+        Symfony::load();
+
+        // Start the request tracing for this request
+        Tracer::start(new EchoExporter());
+    }
 }
