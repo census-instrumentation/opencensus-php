8678c98a90c8e1512d51c354bbe0e1beb624d902
Jeff Ching <chingor@google.com>
2017-11-28 12:49:06 -0800

    Remove unused use statements from AppBundle

--- examples/symfony/src/AppBundle/AppBundle.php
+++ examples/symfony/src/AppBundle/AppBundle.php
@@ -2,11 +2,6 @@
 
 namespace AppBundle;
 
-use OpenCensus\Trace\Exporter\StackdriverExporter;
-use OpenCensus\Trace\Integrations\Mysql;
-use OpenCensus\Trace\Integrations\PDO;
-use OpenCensus\Trace\Integrations\Symfony;
-use OpenCensus\Trace\Tracer;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class AppBundle extends Bundle
