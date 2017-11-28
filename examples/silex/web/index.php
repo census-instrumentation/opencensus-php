928bd94f3c5c8a78e9a81e2c2f97deb779f0d3c6
Jeff Ching <chingor@google.com>
2017-11-28 13:14:40 -0800

    Add a simple fibonacci function to trace.

--- examples/silex/web/index.php
+++ examples/silex/web/index.php
@@ -3,8 +3,17 @@
 require_once __DIR__ . '/../vendor/autoload.php';
 
 // Configure and start the OpenCensus Tracer
+use OpenCensus\Trace\Tracer;
 $exporter = new OpenCensus\Trace\Exporter\StackdriverExporter();
-OpenCensus\Trace\Tracer::start($exporter);
+Tracer::start($exporter);
+
+function fib($n)
+{
+    if ($n < 3) {
+        return $n;
+    }
+    return fib($n - 1) + fib($n - 2);
+}
 
 $app = new Silex\Application();
 
@@ -14,6 +23,12 @@ $app->get('/', function () {
 
 $app->get('/hello/{name}', function ($name) use ($app) {
     return 'Hello ' . $app->escape($name);
+});
+
+$app->get('/fib/{n}', function ($n) use ($app) {
+    $n = (int) $n;
+    $fib = Tracer::inSpan(['name' => 'recursiveFib'], 'fib', [$n]);
+    return sprintf('The %dth Fibonacci number is %d', $n, $fib);
 });
 
 $app->run();
