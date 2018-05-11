5107e5135324f17541ecc8fa48f231e330d9c89f
Jeff Ching <ching.jeff@gmail.com>
2018-05-11 13:42:15 -0700

    Add pgsql integration tests (#187)

--- tests/integration/symfony4/test.sh
+++ tests/integration/symfony4/test.sh
@@ -1,17 +1,22 @@
 #!/bin/bash
+# Copyright 2018 OpenCensus Authors
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
 set -e
 
-if [ -z "${CIRCLE_PR_NUMBER}" ]; then
-    BRANCH="master"
-    REPO="https://github.com/census-instrumentation/opencensus-php"
-else
-    PR_INFO=$(curl "https://api.github.com/repos/${CIRCLE_PROJECT_USERNAME}/${CIRCLE_PROJECT_REPONAME}/pulls/${CIRCLE_PR_NUMBER}")
-    BRANCH=$(echo $PR_INFO | jq -r .head.ref)
-    REPO=$(echo $PR_INFO | jq -r .head.repo.html_url)
-fi
-
 pushd $(dirname ${BASH_SOURCE[0]})
+source ../setup_test_repo.sh
 
 composer create-project --prefer-dist symfony/skeleton symfony ^4.0
 cp -r src tests phpunit.xml.dist symfony/
