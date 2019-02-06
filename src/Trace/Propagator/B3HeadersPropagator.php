c71d7a7ce5c878e75ba598a81657f951f334ee0b
Wojciech Możejko <wojciech.mozejko@schibsted.com>
2019-02-06 09:56:20 +0100

    Set fromHeader to false when headers are missing

--- src/Trace/Propagator/B3HeadersPropagator.php
+++ src/Trace/Propagator/B3HeadersPropagator.php
@@ -27,6 +27,10 @@ class B3HeadersPropagator implements PropagatorInterface
         $sampled = $container[self::X_B3_SAMPLED] ?? null;
         $flags = $container[self::X_B3_FLAGS] ?? null;
 
+        if (!$traceId || !$spanId) {
+            return new SpanContext();
+        }
+
         $enabled = null;
 
         if ($sampled !== null) {
