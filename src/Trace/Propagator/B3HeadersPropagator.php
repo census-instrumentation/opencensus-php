820236f120d6f4196a58305ce3af743224a8af0c
Wojciech Możejko <wojciech.mozejko@schibsted.com>
2019-02-06 09:49:14 +0100

    Add support for booleans in X-B3-Sampled header

--- src/Trace/Propagator/B3HeadersPropagator.php
+++ src/Trace/Propagator/B3HeadersPropagator.php
@@ -30,7 +30,7 @@ class B3HeadersPropagator implements PropagatorInterface
         $enabled = null;
 
         if ($sampled !== null) {
-            $enabled = $sampled === '1';
+            $enabled = ($sampled === '1' || $sampled === 'true');
         }
 
         if ($flags === '1') {
