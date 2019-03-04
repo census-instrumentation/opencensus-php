5dc51db49a510567df81dfdcec95986e75673072
Nenad Stojanovikj <nekk1@live.com>
2019-03-04 16:00:07 +0100

    Add Status constants

--- src/Trace/Status.php
+++ src/Trace/Status.php
@@ -25,6 +25,28 @@ namespace OpenCensus\Trace;
 class Status
 {
     /**
+     * The constants are taken from the Status documentation
+     * https://opencensus.io/tracing/span/status/#status
+     */
+    public const OK = 0;
+    public const CANCELLED = 1;
+    public const UNKNOWN = 2;
+    public const INVALID_ARGUMENT = 3;
+    public const DEADLINE_EXCEEDED = 4;
+    public const NOT_FOUND = 5;
+    public const ALREADY_EXISTS = 6;
+    public const PERMISSION_DENIED = 7;
+    public const RESOURCE_EXHAUSTED = 8;
+    public const FAILED_PRECONDITION = 9;
+    public const ABORTED = 10;
+    public const OUT_OF_RANGE = 11;
+    public const UNIMPLEMENTED = 12;
+    public const INTERNAL = 13;
+    public const UNAVAILABLE = 14;
+    public const DATA_LOSS = 15;
+    public const UNAUTHENTICATED = 16;
+
+    /**
      * @var int The status code
      */
     private $code;
